eafd2c70619094730384381406126e64179bf128
Muti-Kara <muti.kara@ug.bilkent.edu.tr>
2024-07-17 03:25:04 -0400

    waybar added

--- refresh.sh
+++ refresh.sh
@@ -1,18 +1,11 @@
 cp bashrc ~/.bashrc
-cp Xresources ~/.Xresources
-cp libinput-gestures.conf ~/.config
 cp -r ./hypr ~/.config
-cp -r ./bspwm ~/.config
-cp -r ./sxhkd ~/.config
-cp -r ./polybar ~/.config
+cp -r ./waybar ~/.config
 cp -r ./kitty ~/.config
-cp -r ./picom ~/.config
 cp -r ./rofi ~/.config
 
 
 if [ "$EUID" == 0 ]; then
-cp etc.X11.xorg.conf.d.30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
-cp etc.X11.xorg.conf.d.20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
 cp etc.ly.config.ini /etc/ly/config.ini
 echo "For Ly DM run: systemctl restart ly.service"
 fi
